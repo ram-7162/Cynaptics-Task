ddd44c0640d769a41df00706fd47aa60191b1915
ram-7162 <rohitjhajhda@gmail.com>
2026-07-20 00:37:18 +0530

    Update generate.py

--- generate.py
+++ generate.py
@@ -1,6 +1,31 @@
 import torch
-from task1.model import GPTLanguageModel
+from model import GPTLanguageModel
 from transformers import GPT2TokenizerFast
+
+
+
+def generate(model, tokenizer, prompt, max_new_tokens, device):
+
+    model.eval()
+    idx = torch.tensor(encode(prompt), dtype=torch.long).to(device) ## (T, )
+    idx = idx.unsqueeze(0)  ##(1, T)
+    print(decode(idx[0]))
+
+    with torch.no_grad():
+        for _ in range(max_new_tokens):
+            T = idx.shape[-1]
+            idx_cond = idx
+            if(T >= block_size):
+                idx_cond = idx[:, -block_size:]
+            y_pred = model(idx_cond)   ###(1, T, vocab_size)
+            logits = y_pred[:, -1, :]   ###(1, vocab_size)
+            probab = torch.softmax(logits, dim = -1)  ###(1, vocab_size)
+            output = torch.argmax(probab, dim = -1, keepdim = True)  ###(1,1) return argument with max prop
+            idx = torch.cat((idx, output), dim = -1)
+            print(decode(output[0]))
+
+    print("=="*50)
+    print(decode(idx[0]))
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
@@ -12,22 +37,13 @@ decode = lambda l: tokenizer.decode(l)
 
 vocab_size = tokenizer.vocab_size
 
-checkpoint = torch.load("model.pth", map_location=device)
+checkpoint = torch.load("model.pt", map_location=device)
 
 model = GPTLanguageModel(vocab_size).to(device)
 model.load_state_dict(checkpoint["model_state_dict"])
-model.eval()
 
-prompt = "KING:\nMy lord, "
+init_word = input("Enter the starting few words : ")
 
-context = torch.tensor([encode(prompt)], dtype=torch.long, device=device)
+generate(model, tokenizer, init_word, max_new_tokens=100, device=device)
 
-output = model.generate(
-    context,
-    max_new_tokens=500,
-    temperature=0.8,
-    top_k=50,
-    top_p=0.9
-)
 
-print(decode(output[0].tolist()))
