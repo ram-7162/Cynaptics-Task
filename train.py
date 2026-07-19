f9deb70587e495688cda37dec3dd7f87828714ab
ram-7162 <rohitjhajhda@gmail.com>
2026-07-20 01:01:40 +0530

    Update train.py

--- train.py
+++ train.py
@@ -45,6 +45,13 @@ crieterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate, weight_decay=0.01)
 
 
+for module in model.modules():
+    if isinstance(module, nn.Linear):
+        torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+        if module.bias is not None:
+            torch.nn.init.zeros_(module.bias)
+    elif isinstance(module, nn.Embedding):
+        torch.nn.init.normal_(module.weight, mean=0.0, std=0.02
 
 
 
